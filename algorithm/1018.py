db1d56a340a5d67e509e6cc5d20abd7220b49d04
dkahsem27 <dkahsem27@gmail.com>
2023-11-05 15:55:47 +0900

    :memo:update

--- algorithm/1018.py
+++ algorithm/1018.py
@@ -9,6 +9,6 @@ for i in range(a-8):
     for j in range(b-8):
         for r in range(i, i+8):
             for c in range(j, j+8):
-                print(sample1[r]) #(origin[r][c])
+                if sample1[r][c]==origin[r][c]:
             
         
